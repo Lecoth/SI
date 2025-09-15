6bb35d1fd34eaa146883c1d8df3d89e9db820d16
Letícia Paes <163786532+Lecoth@users.noreply.github.com>
2025-09-14 21:35:09 -0300

    Add files via upload

---
+++ LP3/Aula04.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula04;
+
+/**
+ *
+ * @author letic
+ */
+public class Aula04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC", "Amarelo", 0.4f);
+        c1.status();
+    }
+    
+}
---
+++ LP3/Caneta.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula04;
+
+/**
+ *
+ * @author letic
+ */
+public class Caneta {
+    private String modelo;
+    private float ponta;
+    private boolean tampada;
+    private String cor;
+    
+    public Caneta(String m, String c, float p) {
+        this.tampar();
+        this.modelo = m;
+        this.ponta = p;
+        this.cor = c;
+    }
+    
+    public String getModelo() {
+        return this.modelo;
+    }
+    public void setModelo(String m) {
+        this.modelo = m;
+    }
+    public float getPonta() {
+        return this.ponta;
+    }
+    public void setPonta(float p) {
+        this.ponta = p;
+    }
+    public void tampar() {
+        this.tampada = true;
+    }
+    public void destampar() {
+        this.tampada = false;
+    }
+    public void status() {
+        System.out.println("Sobre a caneta:");
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Tampada: " + this.tampada);
+    }
+    
+}
